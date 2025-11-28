e8110a40dc7d7b06bdf67d43461e8902861cd188
stanislavpeshkur <stanislavpeshkur@gmail.com>
2025-11-27 23:06:30 -0700

    added worker: python bot.py

--- bot.py
+++ bot.py
@@ -134,6 +134,7 @@ async def handle_message(msg: types.Message):
 # --- Запуск ---
 async def main():
     print("Бот запущен...")
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
